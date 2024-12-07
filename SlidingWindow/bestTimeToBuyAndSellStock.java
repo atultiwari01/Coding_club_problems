e923d75a8ed77886c3c0b12944b95ba412a373c1
atultiwari01 <atultiwari4950@gmail.com>
2024-12-07 14:08:52 +0530

    move file

--- SlidingWindow/bestTimeToBuyAndSellStock.java
+++ SlidingWindow/bestTimeToBuyAndSellStock.java
@@ -1,26 +1,3 @@
-// class Solution {
-//     public int maxProfit(int[] prices) {
-//         int minProfit= 0;
-//         int maxProfit= 0;
-
-//         for (int i = prices.length; i > prices.length ; i--){
-//             int a = prices[i];
-//             if(maxProfit == 0){
-//                 maxProfit = prices[i];
-//             }
-//             else{
-//                 if(max)
-//             }
-
-//         }
-//     }
-// }
-
-// public class bestTimeToBuyAndSellStock {
-//     public static main
-// }
-
-import java.util.Arrays;
 class Solution {
     public int maxProfit(int[] prices) {
         int maxProfit = 0;

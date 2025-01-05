f601b84cbfc48b341a3a0a7fcc42617433b81793
atultiwari01 <atultiwari4950@gmail.com>
2025-01-05 12:02:10 +0530

    cyclic Sort code

--- CyclicSort/cyclicSort.java
+++ CyclicSort/cyclicSort.java
@@ -1,7 +1,7 @@
 package CyclicSort;
 import java.util.Arrays;
 
-public class cyclicSort{
+class cyclicSort{
     public static void main(String[] args) {
         int arr[] ={3, 5, 2, 1, 4};
         sort(arr);
